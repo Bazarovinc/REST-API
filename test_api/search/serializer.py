89cc8292f5364c091d103fb278d284b1f6420c66
Nikita Veselenko <bazarov228bazarov@yandex.ru>
2020-09-03 11:07:16 +0300

    Change serializer

--- test_api/search/serializer.py
+++ test_api/search/serializer.py
@@ -2,22 +2,11 @@ from rest_framework import serializers
 from .models import User, SearchPoint
 
 
-class UserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=50)
-    x = serializers.IntegerField()
-    y = serializers.IntegerField()
-    description = serializers.CharField(max_length=255)
+class UserSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        return User.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.x = validated_data.get('x', instance.x)
-        instance.y = validated_data.get('y', instance.y)
-        instance.description = validated_data.get('description', instance.description)
-        instance.save()
-        return instance
+    class Meta:
+        model = User
+        fields = ("__all__")
 
 
 class SearchUsersSerializer(serializers.ModelSerializer):
